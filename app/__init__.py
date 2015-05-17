00496a88d9bfd14987c30aaec32a9618535eee1e
njuwangchen <wangchenclark0512@gmail.com>
2015-05-17 16:09:03 +0800

    加入了server-side的session支持。需要安装redis服务器

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import CORS
 from flask.ext.restful import Api
+from flask.ext.session import Session
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,6 +13,7 @@ app.secret_key = 'A0Zr89j/4yX R~XHH!jmN]LWX/,?RT'
 db = SQLAlchemy(app)
 cor = CORS(app, allow_headers='*', supports_credentials=True)
 api = Api(app)
+Session(app)
 
 from app import models
 from app import user_views, order_views, captcha_views
